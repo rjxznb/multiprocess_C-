fdb26322624fa8d06d8f6e975f588d1644f66dff
家兴 任 <2216681472@qq.com>
2025-04-25 16:48:51 +0800

    spinlock

--- Project1/Project1/atomic.cpp
+++ Project1/Project1/atomic.cpp
@@ -1,10 +1,30 @@
 #include<iostream>
+#include<thread>
 #include<atomic>
 using namespace std;
 
+class SpinLock {
+private:
+	atomic_flag m;
+public:
+	SpinLock() : m{ATOMIC_FLAG_INIT} {}
+
+	void lock() {
+		while (1) {
+			if (m.test_and_set(memory_order_acquire) == 0) break;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+	}
+
+	void unlock() {
+		m.clear(memory_order_release);
+	}
+};
+
+
 int main() {
-	atomic<int> a;
-	a.load(std::memory_order_acquire);
-	a.store(std::memory_order_release);
-	aaa
+	
+
+
+
 }
